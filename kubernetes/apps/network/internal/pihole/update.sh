f09e81450d784a205d6bdbc86bbc1987ba1c2ece
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2025-02-05 17:01:13 -0600

    add pihole

---
+++ kubernetes/apps/network/internal/pihole/update.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+# use 1.1.1.1 in case there's no dns container up
+#echo "original resolv:" -n
+#cat /etc/resolv.conf
+#echo ""
+#echo "nameserver 1.1.1.1" > /etc/resolv.conf
+
+if [ -f "/final/gravity.db" ]; then
+	echo "using symbol link for /etc/pihole, as /final is already setup"
+	rm /etc/pihole -rf
+	ln -s /final /etc/pihole
+fi
+
+### Whitelist items
+echo "Nuking existing allowlist"
+pihole -w --nuke
+
+# Add anudeepND's allowlist first, then add these on top of it
+# from https://github.com/anudeepND/whitelist/blob/master/scripts/whitelist.sh#L28
+echo "Downloading anudeepND's allowlist's"
+curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt > /tmp/allow
+curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt >> /tmp/allow
+curl -sS https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt >> /tmp/allow
+
+echo "Adding more domains to allowlist"
+curl -sS https://tholinka.github.io/projects/hosts/allowlist >> /tmp/allow
+
+echo "Dedupping allowlist"
+cat /tmp/allow | sed '/^[[:blank:]]*#/d;s/#.*//' | sed '/^[[:space:]]*$/d' | sort | uniq > /tmp/a
+
+
+echo "Setting allowlist"
+pihole -w $(cat /tmp/a)
+
+echo "Done with allowlist"
+
+
+# new sqlite approach based on https://discourse.pi-hole.net/t/blocklist-management-in-pihole-v5/31971/9
+echo;
+echo;
+echo "removing old adlist"
+sqlite3 /etc/pihole/gravity.db "DELETE FROM adlist"
+
+
+echo "Downloading adlist from wally3k.firebog.net"
+echo;
+curl "https://v.firebog.net/hosts/lists.php?type=tick" | xargs -I {} sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (Address, Comment, Enabled) VALUES ('{}', 'firebog, added `date +%F`', 1);"
+
+echo;
+echo;
+echo "Adding tholinka.github.io tracking lists"
+
+echo "https://tholinka.github.io/projects/hosts/wintracking/normal
+https://tholinka.github.io/projects/hosts/hosts" | xargs -I {} sqlite3 /etc/pihole/gravity.db "INSERT INTO adlist (Address, Comment, Enabled) VALUES ('{}', 'tholinka.github.io, added `date +%F`', 1);"
+
+# let gravity run during startup
+#echo;
+#echo "Running pihole gravity"
+#echo;
+
+#pihole -g
+
+if [ -L /etc/pihole ]; then
+	echo "skipping copying of config, as /etc/pihole is a symlink"
+else
+	echo "copying config to pihole container config"
+	cp -a /etc/pihole/. /final
+fi
+
+# switch dns to pihole
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
