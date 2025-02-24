1bef3560bade4effd1779a51f0d47b1eaaf307da
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2025-02-23 20:46:31 -0600

    move nas bootstrap to its own script

--- bootstrap/scripts/bootstrap-cluster.sh
+++ bootstrap/scripts/bootstrap-cluster.sh
@@ -99,117 +99,6 @@ function fetch_kubeconfig() {
 	popd >/dev/null
 }
 
-# Add NAS to cluster
-# - See https://github.com/siderolabs/talos/issues/3990 for more info.
-# - ensure you do the configuration that's in talos/patches/global manually!
-# 	especially the machine-network and machine-files ones!
-# - ensure HAProxy is setup to mimic KubePrism, or Cilium will fail:
-# 	defaults
-# 		timeout client 10s
-# 		timeout connect 5s
-# 		timeout server 10s
-
-# 	frontend kubeprism
-# 		mode tcp
-# 		bind 127.0.0.1:7445
-# 		default_backend k8s_api
-
-# 	backend k8s_api
-# 		mode tcp
-# 		server lb 192.168.20.2:6443 check
-# 		server c1 192.168.20.61:6443 check backup
-# 		server c2 192.168.20.62:6443 check backup
-# 	server c3 192.168.20.63:6443 check backup
-function add_nas_to_cluster() ( #subshell
-	source "$(dirname "${0}")/lib/common.sh"
-
-	function do_ssh() {
-		ssh -t nas.servers.internal "$@"
-	}
-
-	if ! controller=$(talosctl config info --output json | jq --exit-status --raw-output '.endpoints[]' | shuf -n 1) || [[ -z "${controller}" ]]; then
-		log error "No Talos controller found"
-	fi
-
-	local CLUSTER_DOMAIN
-	CLUSTER_DOMAIN="$(talosctl -n "$controller" get kubeletconfig -o jsonpath="{.spec.clusterDomain}")"
-	local CLUSTER_DNS
-	CLUSTER_DNS="$(talosctl -n "$controller" get kubeletconfig -o jsonpath="{.spec.clusterDNS}")"
-	local SOCKET_PATH
-	SOCKET_PATH=/var/run/containerd/containerd.sock
-	local TMPDIR
-	TMPDIR=$(mktemp -d)
-
-	function do_talos() {
-		talosctl -n "$controller" "$@"
-	}
-
-	# wait for nas to be up
-	until do_ssh -o ConnectTimeout=1 'exit 0';
-		log info "NAS not online, sleeping for 5 seconds..."
-		do sleep 5;
-	done
-
-	## remove existing
-
-	set -x
-	# this can fail
-	# shellcheck disable=SC2015
-	do_ssh 'systemctl is-active kubelet.service containerd.service 1>/dev/null && \
-	sudo systemctl disable kubelet.service containerd.service && \
-	sudo reboot' && sleep 10 || true
-
-	set +x
-	until do_ssh -o ConnectTimeout=1 'exit 0';
-		log info "NAS not online, sleeping for 5 seconds..."
-		do sleep 5;
-	done
-	set -x;
-
-	do_ssh 'sudo find /var/lib/containerd/io.containerd.snapshotter.v1.btrfs/snapshots/ -maxdepth 1 -type d -exec btrfs subvolume delete {} \; ; \
-		sudo rm -rf \
-		/var/lib/containerd /var/lib/kubelet /var/lib/cni /var/local/openebs \
-		/etc/kubernetes /etc/cni \
-		/opt/cni'
-
-	## copy files from talos cluster
-	do_talos cat /etc/kubernetes/kubeconfig-kubelet > "$TMPDIR/kubelet.conf"
-	do_talos cat /etc/kubernetes/bootstrap-kubeconfig > "$TMPDIR/bootstrap-kubelet.conf"
-	do_talos cat /etc/kubernetes/pki/ca.crt > $TMPDIR/ca.crt
-	sed -i "/server:/ s|:.*|: https://192.168.20.2:6443|g" "$TMPDIR/kubelet.conf" "$TMPDIR/bootstrap-kubelet.conf"
-	cat > "$TMPDIR/var-lib-kubelet-config.yaml" <<- _EOT
-		kind: KubeletConfiguration
-		apiVersion: kubelet.config.k8s.io/v1beta1
-		authentication:
-			anonymous:
-			enabled: false
-			webhook:
-			enabled: true
-			x509:
-			clientCAFile: /etc/kubernetes/pki/ca.crt
-		authorization:
-			mode: Webhook
-		clusterDomain: "$CLUSTER_DOMAIN"
-		clusterDNS: $CLUSTER_DNS
-		runtimeRequestTimeout: "0s"
-		cgroupDriver: systemd
-		containerRuntimeEndpoint: unix:/$SOCKET_PATH
-	_EOT
-	cat > "$TMPDIR/kubelet.env" <<- _EOT
-		KUBELET_ARGS="--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --config=/var/lib/kubelet/config.yaml --fail-swap-on=false"
-	_EOT
-	scp -r "$TMPDIR" "nas.servers.internal:$TMPDIR"
-	do_ssh "sudo mkdir -p /etc/kubernetes/pki /var/lib/kubelet &&
-		sudo mv $TMPDIR/kubelet.conf /etc/kubernetes/kubelet.conf &&
-		sudo mv $TMPDIR/bootstrap-kubelet.conf /etc/kubernetes/bootstrap-kubelet.conf &&
-		sudo mv $TMPDIR/ca.crt /etc/kubernetes/pki/ca.crt &&
-		sudo mv $TMPDIR/var-lib-kubelet-config.yaml /var/lib/kubelet/config.yaml &&
-		sudo mv $TMPDIR/kubelet.env /etc/kubernetes/kubelet.env &&
-		sudo systemctl enable --now containerd.service kubelet.service"
-
-	set +x
-)
-
 # Talos requires the nodes to be 'Ready=False' before applying resources
 function wait_for_nodes() {
 	log debug "Waiting for nodes to be available"
@@ -321,9 +210,6 @@ function main() {
 	apply_talos_config
 	bootstrap_talos
 	fetch_kubeconfig
-	if [[ $ADD_NAS == 'true' ]]; then
-		add_nas_to_cluster
-	fi
 
 	# Apply resources and Helm releases
 	wait_for_nodes
